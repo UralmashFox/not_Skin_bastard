7436c3a6b3dbb0a0988fce7386df1b9315909a96
UralmashFox <rikch_zurps@mail.ru>
2019-11-10 17:44:38 +0300

    Cool! Now update all my work with camera

--- spot/src/thespot_python/scripts/main.py
+++ spot/src/thespot_python/scripts/main.py
@@ -3,6 +3,8 @@
 import rospy
 import math
 from std_msgs.msg import Float64
+import rospy
+from std_msgs.msg import String
 
 def talker():
     pubLBB = rospy.Publisher('/thespot_robot/left_back_big/command', Float64, queue_size=10)
@@ -14,7 +16,7 @@ def talker():
     pubRFS = rospy.Publisher('/thespot_robot/right_forward_small/command', Float64, queue_size=10)
     pubLFS = rospy.Publisher('/thespot_robot/left_forward_small/command', Float64, queue_size=10)
     pubRBS = rospy.Publisher('/thespot_robot/right_back_small/command', Float64, queue_size=10)
-
+    print("HELLO")
 
     rospy.init_node('thespot_talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
@@ -36,7 +38,7 @@ def talker():
         positionRBB = -math.sin(angle)
         rospy.loginfo(positionRBB)
         pubRBB.publish(positionRBB)
-#Small legz:
+        #Small legz:
 
         positionLBS = math.cos(angle)
         rospy.loginfo(positionLBS)
@@ -54,13 +56,31 @@ def talker():
         rospy.loginfo(positionRBS)
         pubRBS.publish(positionRBS)
 
-
-
         rate.sleep()
-        angle += 0.9
+        angle += 0.5
+
+""" def callback(data):
+    rospy.loginfo("I heard %s",data.data)
+    print m._connection_header
+    {'callerid': '/talker_38321_1284999593611',
+    'latching': '0',
+    'md5sum': '992ce8a1687cec8c8bd883ec73ca41d1',
+    'message_definition': 'string data\n\n',
+    'topic': '/chatter',
+    'type': 'std_msgs/String'}
+    
+def listener():
+    rospy.init_node('thespot_talker')
+    rospy.Subscriber("chatter", String, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin() """
 
 if __name__ == '__main__':
     try:
         talker()
+        #listener()
     except rospy.ROSInterruptException:
         pass
+
+
+
